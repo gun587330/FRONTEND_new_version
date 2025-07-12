d579dd5853f4769f2bb8fb705bc6f21e435c1ce6
gun587330 <gun5873@naver.com>
2025-07-12 19:43:32 +0900

    feat: 04-02-counter파일 추가(next에서 state 작동 실험)

---
+++ class-example/my-app/src/app/section04/04-02-counter/page.tsx
@@ -0,0 +1,28 @@
+"use client"; // React 18 버전 이후로는 이 녀석을 써줘야 useState 쓸 때 에러가 없음!
+import { useState } from "react";
+
+const 카운터 = () => {
+    // Fullname
+    // window.React.useState()
+    const [카운트변수, 카운트바꿔주는함수] = useState(0)
+
+    const 카운트올리기 = () => {
+        카운트바꿔주는함수(카운트변수 + 1)
+    }
+
+    const 카운트내리기 = () => {
+        카운트바꿔주는함수(카운트변수 - 1)
+    }
+
+    // 함수의 리턴값은 1개만 가능 => 하나로 묶어서 리턴해야 함.
+    return (
+        // <> </> : React.Fragment(프래그먼트)
+        <>
+            <div>{카운트변수}</div>
+            <button onClick={카운트올리기}>카운트 올리기!!</button>
+            <button onClick={카운트내리기}>카운트 내리기!!</button>
+        </>
+    )
+}
+
+export default 카운터;
