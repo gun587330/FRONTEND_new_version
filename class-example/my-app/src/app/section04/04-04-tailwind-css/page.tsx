6546891d60e13054386b20fa27b40295913d884a
gun587330 <gun5873@naver.com>
2025-07-13 19:17:00 +0900

    refactor: tailwind V4버전 문법 적용

--- class-example/my-app/src/app/section04/04-04-tailwind-css/page.tsx
+++ class-example/my-app/src/app/section04/04-04-tailwind-css/page.tsx
@@ -3,13 +3,15 @@
 import styles from "./styles.module.css";
 
 export default function CssPage(){
-    //빈 태그로 묶어주는 걸 fragment라고 한다!!
+    // 빈 태그로 묶어주는 걸 fragment라고 한다!!
     return (
         <>
             <button className={styles.버튼스타일}>버튼</button>
-            <div className={styles.네모상자스타일}>네모상자</div>
-            {/*tailwind의 또 다른 사용법*/}
-            <div className={"bg-green-200"}>버튼2</div>
+            <div className={styles.네모상자스타일1}>네모상자</div>
+            <br />
+            <div className={styles.네모상자스타일2}>네모상자</div>
+            <div className="lg:chulsuBox">클때는 초록색</div>
+            <div className="lg:text-green-900">클때는 초록색</div>
         </>
     );
 };
